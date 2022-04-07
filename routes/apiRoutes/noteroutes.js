6ee01cf081f14b9466ba6405e35346780b2fb25e
Xcamaj <Xhemajlcamaj@gmail.com>
2022-04-07 16:02:50 -0400

    tasks now save

--- routes/apiRoutes/noteroutes.js
+++ routes/apiRoutes/noteroutes.js
@@ -1,20 +1,29 @@
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const { note } = require('../../db/db.json')
-const { createNote } = require('../../lib/notes')
+const { createNote, findById, editNote, removeNote } = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
-    res.json(note)
-})
+    res.json(note);
+});
 
 router.post('/notes', (req, res) => {
+
     if (!req.body.id) {
         req.body.id = uuidv4();
         createNote(req.body, note);
-    }else {
-        editNote(req.body, note)
+    } else {
+        editNote(req.body, note);
     }
+
     res.json(req.body);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    const note = findById(req.params.id, note);
+
+    removeNote(note, note);
+    res.json();
 });
 
 module.exports = router;
