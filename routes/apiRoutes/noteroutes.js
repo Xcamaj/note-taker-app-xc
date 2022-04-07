ad3609448493315f37c551511e3f55f38cc9b030
Xcamaj <Xhemajlcamaj@gmail.com>
2022-04-07 15:44:55 -0400

    change server

--- routes/apiRoutes/noteroutes.js
+++ routes/apiRoutes/noteroutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
 const { note } = require('../../db/db.json')
 const { createNote } = require('../../lib/notes')
 
@@ -8,8 +9,12 @@ router.get('/notes', (req, res) => {
 
 router.post('/notes', (req, res) => {
     if (!req.body.id) {
+        req.body.id = uuidv4();
         createNote(req.body, note);
+    }else {
+        editNote(req.body, note)
     }
-})
+    res.json(req.body);
+});
 
 module.exports = router;
